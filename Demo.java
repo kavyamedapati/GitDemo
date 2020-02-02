0d9093ac0c960988ca940b20c18a39b3d9aac234
kavyamedapati <medapati.kavya07@gmail.com>
2020-02-02 17:29:26 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,10 @@
+package com.rameshsoft.git;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("hi ");
+		System.out.println("hello");
+		System.out.println("kavya");
+	}
+
+}
