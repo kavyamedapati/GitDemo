5e1104ea9a294769483b6b837ee0e8e53f474933
kavyamedapati <medapati.kavya07@gmail.com>
2020-02-02 18:20:25 +0530

    second commit

---
+++ Hello.java
@@ -0,0 +1,10 @@
+package com.rameshsoft.git;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Iam kavya");
+		System.out.println("Iam a software");
+		System.out.println("my father name is Srinivasa Rao ");
+	}
+
+}
